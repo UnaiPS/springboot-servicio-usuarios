b37ab73f773ff8272e2018d54b91319b3c609144
Unai Perez <uperez@certiorem.com>
2020-04-02 14:05:25 +0200

    añadida trazabilidad

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$m1n.KD1sxQISqVI/0fNL4e1Wo.OjtHnBfjfGJRB5NNVFkd74yhSL2', 1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$T2Jmqp9VANoZwkzR9Y81a.So0361068RSM7upjdEGEyXJcKi1B9au', 1, 'Jhon', 'Doe', 'jhon.doe@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$m1n.KD1sxQISqVI/0fNL4e1Wo.OjtHnBfjfGJRB5NNVFkd74yhSL2', true, 'Andres', 'Guzman', 'profesor@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$T2Jmqp9VANoZwkzR9Y81a.So0361068RSM7upjdEGEyXJcKi1B9au', true, 'Jhon', 'Doe', 'jhon.doe@bolsadeideas.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
